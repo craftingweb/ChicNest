fa67c3219a26a70dcb64e449a9ccd4d9d8a7c282
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-11-28 16:36:07 -0500

    drop down menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
-})
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu__active");
+  });
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu__active");
+  });
+});
