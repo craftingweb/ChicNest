4212805468b7b71be1efc6954344b65a0de05952
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-11-30 18:40:23 -0500

    filter mix it up

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,16 @@ $(function () {
   $(".rightside-menu__close").on("click", function () {
     $(".rightside-menu").removeClass("rightside-menu__active");
   });
+
   $(".top__slider").slick({
     dots: true,
     arrows: false,
     fade: true,
+  });
+
+  var mixer = mixitup(".gallery__inner", {
+    load: {
+      filter: ".living",
+    },
   });
 });
